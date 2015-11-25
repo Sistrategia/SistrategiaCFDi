f283a27eac15fb30e6f7194a6806b2e33cc3051f
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2015-11-25 12:44:11 -0600

    Adding JQuery, Bootstrap, and Owin.

---
+++ src/Sistrategia.SAT.CFDiWebSite/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace Sistrategia.SAT.CFDiWebSite
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class Startup
+    {
+        public AppFunc Configuration() {
+            return async env => {
+                var writer = new StreamWriter((Stream)env["owin.ResponseBody"]);
+                await writer.WriteAsync("SistrategiaCFDi");
+                await writer.FlushAsync();
+            };
+        }
+    }
+}
