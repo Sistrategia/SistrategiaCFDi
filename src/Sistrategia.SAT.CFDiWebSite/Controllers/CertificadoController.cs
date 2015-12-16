881cbc3caed6352dcb4e813e26dfae9b5a393157
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2015-12-16 11:28:58 -0600

    Adding Certificado Controller, Index and Details View.

---
+++ src/Sistrategia.SAT.CFDiWebSite/Controllers/CertificadoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sistrategia.SAT.CFDiWebSite.Models;
+
+namespace Sistrategia.SAT.CFDiWebSite.Controllers
+{
+    public class CertificadoController : BaseController
+    {
+        // GET: Certificado
+        public ActionResult Index()
+        {
+            var model = new CertificadoIndexViewModel {
+                Certificados = this.DBContext.Certificados.ToList()
+            };
+            return View(model);
+        }
+
+        public ActionResult Create() {
+            var model = new CertificadoCreateViewModel();
+            return View(model);
+        }
+    }
+}
---
+++ src/Sistrategia.SAT.CFDiWebSite/Models/CertificadoViewModels.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+//using Microsoft.Owin.Security;
+using Sistrategia.SAT.Resources;
+using Sistrategia.SAT.CFDiWebSite.CFDI;
+using System.Web;
+
+namespace Sistrategia.SAT.CFDiWebSite.Models
+{
+    public class CertificadoIndexViewModel
+    {
+        public CertificadoIndexViewModel() {
+            this.Certificados = new List<Certificado>();
+        }
+
+        public List<Certificado> Certificados { get; set; }
+    }
+
+    public class CertificadoCreateViewModel
+    {
+        public CertificadoCreateViewModel() {
+        }
+
+        [Required]
+        [Display(Name = "Número de serie")]
+        public string NumSerie { get; set; }
+
+        [Required]
+        [Display(Name = "R.F.C.")]
+        public string RFC { get; set; }
+
+        [Required]
+        [Display(Name = "Inicia")]
+        public string Inicia { get; set; }
+
+        [Required]
+        [Display(Name = "Finaliza")]
+        public string Finaliza { get; set; }
+
+        [Display(Name = "Certificado")]
+        public string CertificadoBase64 { get; set; }
+
+        //[Display(Name = "Archivo PFX")]
+        //public byte[] PFXArchivo { get; set; }
+        [Required, DataType(DataType.Upload), Display(Name = "Archivo PFX")]
+        public HttpPostedFileBase PFXArchivo { get; set; }
+
+        [Required]
+        [Display(Name = "Contraseña del PFX")]
+        public string PFXContrasena { get; set; }
+
+        //[Required]
+        [Display(Name = "Estado")]
+        public string Estado { get; set; }
+        
+
+        //[Required]
+        //[Display(ResourceType = typeof(LocalizedStrings), Name = "FiscalNameField", ShortName = "Name")]
+        //public string Nombre { get; set; }
+    }
+}
