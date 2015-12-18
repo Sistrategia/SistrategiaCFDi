3abc25d8e81c2cb9920a625781a181425563979c
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2015-12-18 08:37:37 -0600

    Adding web service client proxy for EDICOM.

---
+++ src/Sistrategia.SAT.CFDiWebSite/CFDI/EDICOM/CFDiService.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Web.Services.Description;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+using System.Text;
+
+namespace Sistrategia.SAT.CFDiWebSite.CFDI.EDICOM
+{
+    [DesignerCategory("code")]
+    [WebServiceBinding(Name = "CFDiSoapBinding", Namespace = "http://cfdi.service.ediwinws.edicom.com")]
+    internal class CFDiService : SoapHttpClientProtocol, ICFDIService
+    {
+        private bool useDefaultCredentialsSetExplicitly;
+
+        public CFDiService() {
+            //this.Url = global::TestCFDiClient.Properties.Settings.Default.TestCFDiClient_edicom_CFDiService;
+            //this.Url = @"https://cfdiws.sedeb2b.com/EdiwinWS/services/CFDi"; // System.Configuration.AppSettingsSection
+            this.Url = System.Configuration.ConfigurationManager.AppSettings["cfdiRemoteService"].ToString();
+            if ((this.IsLocalFileSystemWebService(this.Url) == true)) {
+                this.UseDefaultCredentials = true;
+                this.useDefaultCredentialsSetExplicitly = false;
+            }
+            else {
+                this.useDefaultCredentialsSetExplicitly = true;
+            }
+        }
+
+        public new string Url {
+            get { return base.Url; }
+            set {
+                if ((((this.IsLocalFileSystemWebService(base.Url) == true)
+                            && (this.useDefaultCredentialsSetExplicitly == false))
+                            && (this.IsLocalFileSystemWebService(value) == false))) {
+                    base.UseDefaultCredentials = false;
+                }
+                base.Url = value;
+            }
+        }
+
+        public new bool UseDefaultCredentials {
+            get { return base.UseDefaultCredentials; }
+            set {
+                base.UseDefaultCredentials = value;
+                this.useDefaultCredentialsSetExplicitly = true;
+            }
+        }
+
+        #region ICFDIService Implementation
+
+        public byte[] GetCFDI(string user, string password, byte[] file) {
+            return this.getCfdi(user, password, file);
+        }
+
+        public byte[] GetTimbreCFDI(string user, string password, byte[] file) {
+            return this.getTimbreCfdi(user, password, file);
+        }
+
+        public ICancelaResponse CancelaCFDI(string user, string password, string rfc, string[] uuid, byte[] pfx, string pfxPassword) {
+            //return this.cancelaCFDi(user, password, rfc, uuid, pfx, pfxPassword);
+
+            CancelaResponse response = this.cancelaCFDi(user, password, rfc, uuid, pfx, pfxPassword);
+
+            CancelaResponseBase responseBase = new CancelaResponseBase();
+            responseBase.Ack = response.ack;
+            responseBase.Text = response.text;
+
+            System.Collections.Generic.List<string> uuidsList = new System.Collections.Generic.List<string>();
+
+            foreach (string uuidItem in response.uuids) {
+                uuidsList.Add(uuidItem);
+            }
+            responseBase.UUIDs = uuidsList.ToArray();
+
+            System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(response.GetType());
+            MemoryStream ms = new MemoryStream();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Encoding = new UTF8Encoding();
+            XmlWriter xmlWriter = XmlWriter.Create(ms, settings);
+            x.Serialize(xmlWriter, response);
+            string xmlContent = Encoding.UTF8.GetString(ms.GetBuffer());
+
+            responseBase.XmlResponse = xmlContent;
+
+            return responseBase;
+        }
+
+        #endregion
+
+        #region Web Service Calls
+
+        [SoapDocumentMethod("", RequestNamespace = "http://cfdi.service.ediwinws.edicom.com", 
+                                ResponseNamespace = "http://cfdi.service.ediwinws.edicom.com", 
+                                Use = SoapBindingUse.Literal, 
+                                ParameterStyle = SoapParameterStyle.Wrapped)]
+        [return: XmlElement("getCfdiReturn", DataType = "base64Binary")]
+        public byte[] getCfdi(string user, string password, [XmlElement(DataType = "base64Binary")] byte[] file) {
+            object[] results = this.Invoke("getCfdi", new object[] {
+                        user,
+                        password,
+                        file});
+            return ((byte[])(results[0]));
+        }
+
+        [SoapDocumentMethod("", RequestNamespace = "http://cfdi.service.ediwinws.edicom.com",
+                                ResponseNamespace = "http://cfdi.service.ediwinws.edicom.com",
+                                Use = SoapBindingUse.Literal,
+                                ParameterStyle = SoapParameterStyle.Wrapped)]
+        [return: XmlElement("getTimbreCfdiReturn", DataType = "base64Binary")]
+        public byte[] getTimbreCfdi(string user, string password, [XmlElement(DataType = "base64Binary")] byte[] file) {
+            object[] results = this.Invoke("getTimbreCfdi", new object[] {
+                        user,
+                        password,
+                        file});
+            return ((byte[])(results[0]));
+        }
+
+        [SoapDocumentMethod("", RequestNamespace = "http://cfdi.service.ediwinws.edicom.com",
+                                ResponseNamespace = "http://cfdi.service.ediwinws.edicom.com",
+                                Use = SoapBindingUse.Literal,
+                                ParameterStyle = SoapParameterStyle.Wrapped)]
+        [return: XmlElement("cancelaCFDiReturn")]
+        public CancelaResponse cancelaCFDi(string user, string password, string rfc, [XmlElement("uuid")] string[] uuid, [XmlElement(DataType = "base64Binary")] byte[] pfx, string pfxPassword) {
+            object[] results = this.Invoke("cancelaCFDi", new object[] {
+                        user,
+                        password,
+                        rfc,
+                        uuid,
+                        pfx,
+                        pfxPassword});
+            return ((CancelaResponse)(results[0]));
+        }
+
+        [SoapDocumentMethod("", RequestNamespace = "http://cfdi.service.ediwinws.edicom.com",
+                                ResponseNamespace = "http://cfdi.service.ediwinws.edicom.com",
+                                Use = SoapBindingUse.Literal,
+                                ParameterStyle = SoapParameterStyle.Wrapped)]
+        [return: XmlElement("cancelaCFDiSignedReturn")]
+        public CancelaResponse cancelaCFDiSigned(string user, string password, [XmlElement(DataType = "base64Binary")] byte[] sign) {
+            object[] results = this.Invoke("cancelaCFDiSigned", new object[] {
+                        user,
+                        password,
+                        sign});
+            return ((CancelaResponse)(results[0]));
+        }
+
+        [SoapDocumentMethod("", RequestNamespace = "http://cfdi.service.ediwinws.edicom.com",
+                                ResponseNamespace = "http://cfdi.service.ediwinws.edicom.com",
+                                Use = SoapBindingUse.Literal,
+                                ParameterStyle = SoapParameterStyle.Wrapped)]
+        [return: XmlElement("getCfdiFromUUIDReturn", DataType = "base64Binary")]
+        public byte[] getCfdiFromUUID(string user, string password, string rfc, [XmlElement("uuid")] string[] uuid) {
+            object[] results = this.Invoke("getCfdiFromUUID", new object[] {
+                        user,
+                        password,
+                        rfc,
+                        uuid});
+            return ((byte[])(results[0]));
+        }
+
+        #endregion
+
+        #region Utilities
+
+        private bool IsLocalFileSystemWebService(string url) {
+            if (((url == null) || (url == string.Empty))) {
+                return false;
+            }
+            System.Uri wsUri = new System.Uri(url);
+            if (((wsUri.Port >= 1024)
+                        && (string.Compare(wsUri.Host, "localHost", System.StringComparison.OrdinalIgnoreCase) == 0))) {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        
+    }
+
+    [Serializable]
+    [DesignerCategory("code")]
+    [XmlType(Namespace = "http://cfdi.service.ediwinws.edicom.com")]
+    public partial class CancelaResponse
+    {
+        private string ackField;
+        private string textField;
+        private string[] uuidsField;
+
+        [XmlElement(IsNullable = true)]
+        public string ack {
+            get { return this.ackField; }
+            set { this.ackField = value; }
+        }
+
+        [XmlElement(IsNullable = true)]
+        public string text {
+            get { return this.textField; }
+            set { this.textField = value; }
+        }
+
+        [XmlArray(IsNullable = true)]
+        [XmlArrayItem("item", IsNullable = false)]
+        public string[] uuids {
+            get { return this.uuidsField; }
+            set { this.uuidsField = value; }
+        }
+    }
+}
--- src/Sistrategia.SAT.CFDiWebSite/SATManager.cs
+++ src/Sistrategia.SAT.CFDiWebSite/SATManager.cs
@@ -12,6 +12,7 @@ namespace Sistrategia.SAT.CFDiWebSite
     public class SATManager
     {
 
+        #region Utilities
         internal static string NormalizeWhiteSpace(string S) {
             if (string.IsNullOrEmpty(S))
                 return S;
@@ -44,5 +45,260 @@ namespace Sistrategia.SAT.CFDiWebSite
             }
             return sb.ToString();
         }
+
+        #endregion
+
+        
+
+
     }
 }
+
+#region Code To Review
+
+
+//public class HomeController : Controller
+//    {
+//        // GET: Home
+//        public ActionResult Index()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        public ActionResult Index(FormCollection post, HttpPostedFileBase xml_file)
+//        {
+//            try
+//            {
+//                string invoicesPath = Server.MapPath(Url.Content("~/FileInvoices/"));
+//                string invoiceFileName = DateTime.Now.ToString("yyyyMMddHmmss_comprobante");
+
+//                // Guardar el comprobante con cadena y sello
+//                string xmlFullPath = invoicesPath + invoiceFileName + "_send.xml";
+//                xml_file.SaveAs(xmlFullPath);
+
+//                // Comprimir y enviar al servicio web
+//                Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile();
+//                string saveToFilePath = invoicesPath + invoiceFileName + "_send.zip";
+//                zip.AddFile(xmlFullPath, "");
+//                zip.Save(saveToFilePath);
+
+//                string filePath = saveToFilePath;
+//                string responsePath = invoicesPath + invoiceFileName + "_response.zip";
+
+//                //byte[] response = new byte[0]; 
+                
+//                byte[] response = GetCFDI(
+//                        ConfigurationManager.AppSettings["EDICOM_user"],
+//                        ConfigurationManager.AppSettings["EDICOM_password"],
+//                        filePath, responsePath);                   
+                
+              
+//                Ionic.Zip.ZipFile zipR = Ionic.Zip.ZipFile.Read(invoicesPath + invoiceFileName + "_response.zip");
+//                zipR.ExtractAll(invoicesPath, Ionic.Zip.ExtractExistingFileAction.OverwriteSilently);
+//                zipR.Dispose();
+
+//                TempData["success"] = "¡XML timbrado!";
+
+//            }
+//            catch (Exception ex)
+//            {
+//                TempData["error"] = ex.Message.ToString();
+//            }
+
+//            return View();
+//        }
+
+//        public static ICFDIService CreateCFDIService() {
+//            return CreateCFDIService("cfdiServiceTest");
+//        }
+
+//        public static ICFDIService CreateCFDIService(string serviceName) {
+//            string serviceClass = System.Configuration.ConfigurationManager.AppSettings[serviceName].ToString();
+
+//            switch (serviceClass) {
+//                case "Sistrategia.Server.SAT.EDICOM.CFDI.CFDiService":
+//                    return new EDICOM.CFDI.CFDiService();                   
+//                case "Sistrategia.Server.SAT.EDICOM.TestCFDI.CFDiService":
+//                default:
+//                    return new EDICOM.TestCFDI.CFDiService();                   
+//            }
+//        }
+
+//        public static byte[] GetCFDI(string user, string password, byte[] file) {
+//            ICFDIService service = CreateCFDIService(); // new EDICOM.TestCFDI.CFDiService();
+//            return service.GetCFDI(user, password, file);
+//        }
+
+//        public static byte[] GetCFDI(string user, string password, string requestFilePath, string responseFilePath) {
+//            ICFDIService service = new EDICOM.TestCFDI.CFDiService();
+
+//            byte[] file = null;
+//            byte[] response = null;
+
+//            System.IO.FileStream fileStream = new System.IO.FileStream(requestFilePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+//            try {
+//                int lenght = (int)fileStream.Length;
+//                file = new byte[lenght];
+//                int count;
+//                int sum = 0;
+
+//                while ((count = fileStream.Read(file, sum, lenght - sum)) > 0)
+//                    sum += count;
+//            }
+//            finally {
+//                fileStream.Close();
+//            }
+
+//            try {
+//                response = GetCFDI(user, password, file);
+//                // response = GetCFDI("USUARIO", "PASSWORD", file);
+//                // Response.Write("Respuesta OK" + response.ToString());
+//            }
+
+//            catch (Exception ex) {
+//                // Response.Write(ex.Message.ToString());
+//                ex.Message.ToString();
+//                throw;
+//            }
+
+//            if (response != null) {
+//                System.IO.FileStream fileStream2 = System.IO.File.Create(responseFilePath);
+//                fileStream2.Write(response, 0, response.Length);
+//                fileStream2.Close();
+//            }
+
+//            return response;
+
+//            //return service.GetCFDI(user, password, file);
+
+
+//        }
+
+
+
+//        //public static ICFDIService CreateCFDIService()
+//        //{
+
+//        //    return CreateCFDIService("cfdiServiceTest");
+//        //    // return CreateCFDIService("Sistrategia.Server.SAT.EDICOM.TestCFDI.CFDiService");
+//        //}
+
+//        //public static ICFDIService CreateCFDIService(string serviceName)
+//        //{
+//        //    string serviceClass = System.Configuration.ConfigurationManager.AppSettings[serviceName].ToString();
+
+//        //    switch (serviceClass)
+//        //    {
+//        //        case "Sistrategia.Server.SAT.EDICOM.CFDI.CFDiService":
+//        //            return new Sistrategia.Server.SAT.EDICOM.CFDI.CFDiService();
+//        //            break;
+//        //        case "Sistrategia.Server.SAT.EDICOM.TestCFDI.CFDiService":
+//        //        default:
+//        //            return new Sistrategia.Server.SAT.EDICOM.TestCFDI.CFDiService();
+//        //            break;
+//        //    }
+//        //}
+
+//        //public static byte[] GetCFDI(string user, string password, string requestFilePath, string responseFilePath)
+//        //{
+//        //    Sistrategia.Server.SAT.CFDI.ICFDIService service = Sistrategia.Server.SAT.CFDI.CFDIServiceFactory.CreateCFDIService();
+
+//        //    byte[] file = null;
+//        //    byte[] response = null;
+
+//        //    System.IO.FileStream fileStream = new System.IO.FileStream(requestFilePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+//        //    try
+//        //    {
+//        //        int lenght = (int)fileStream.Length;
+//        //        file = new byte[lenght];
+//        //        int count;
+//        //        int sum = 0;
+
+//        //        while ((count = fileStream.Read(file, sum, lenght - sum)) > 0)
+//        //            sum += count;
+//        //    }
+//        //    finally
+//        //    {
+//        //        fileStream.Close();
+//        //    }
+
+//        //    try
+//        //    {
+//        //        response = Sistrategia.Server.SAT.SATManager.GetCFDI("USUARIO", "PASSWORD", file);
+//        //        // Response.Write("Respuesta OK" + response.ToString());
+//        //    }
+
+//        //    catch (Exception ex)
+//        //    {
+//        //        // Response.Write(ex.Message.ToString());
+//        //        ex.Message.ToString();
+//        //        throw;
+//        //    }
+
+//        //    if (response != null)
+//        //    {
+//        //        System.IO.FileStream fileStream2 = System.IO.File.Create(responseFilePath);
+//        //        fileStream2.Write(response, 0, response.Length);
+//        //        fileStream2.Close();
+//        //    }
+
+//        //    return service.GetCFDI(user, password, file);
+
+
+//        //}
+
+//        //public static byte[] GetCFDI(string user, string password, byte[] file)
+//        //{
+//        //    Sistrategia.Server.SAT.CFDI.ICFDIService service = Sistrategia.Server.SAT.CFDI.CFDIServiceFactory.CreateCFDIService();
+//        //    return service.GetCFDI(user, password, file);
+//        //}
+//    }
+//}
+
+
+
+
+
+
+//@{
+//    ViewBag.Title = "Index";
+//}
+
+
+//    <div class="col-md-8 col-md-offset-2">
+//        <div class="panel panel-default">
+//            <div class="panel-heading"><h3 class="panel-title text-center">Nuevo timbrado</h3></div>
+//            <div class="panel-body">
+                
+//                @if (TempData["error"] != null)
+//                {
+//                    @Html.Raw("<div class='alert alert-danger text-center'><span class='glyphicon glyphicon-warning-sign'></span> " + TempData["error"] + "</div>")
+//                }
+//                else if (TempData["success"] != null)
+//                {
+//                    @Html.Raw("<div class='alert alert-success text-center'>" + TempData["success"] + "</div>")
+//                }
+//                <form method="post" class="form-horizontal" role="form" enctype="multipart/form-data">                  
+
+//                    <div class="form-group">
+//                        <label for="xml_file" class="col-sm-3 control-label">Archivo XML *</label>
+//                        <div class="col-sm-9">
+//                            <input id="xml_file" type="file" name="xml_file" required />
+//                        </div>
+//                    </div>
+
+//                    <div class="form-group">
+//                        <div class="col-sm-offset-4 col-sm-10">                            
+//                            <button type="submit" class="btn btn-primary">Timbrar</button>
+//                        </div>
+//                    </div>
+
+//                </form>
+
+//            </div>
+//        </div>
+//    </div>
+
+
+#endregion
