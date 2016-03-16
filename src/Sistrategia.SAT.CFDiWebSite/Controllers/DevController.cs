437c4fdfebe2aa9a985b61a6c06dd6acb433956e
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2016-03-16 17:13:21 -0600

    Adding basic TimbradoLog.

--- src/Sistrategia.SAT.CFDiWebSite/Controllers/DevController.cs
+++ src/Sistrategia.SAT.CFDiWebSite/Controllers/DevController.cs
@@ -5,18 +5,54 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Sistrategia.SAT.CFDiWebSite.CloudStorage;
+using Sistrategia.SAT.CFDiWebSite.Models;
+
 namespace Sistrategia.SAT.CFDiWebSite.Controllers
 {
     [Authorize]
     public class DevController : BaseController
     {
         // GET: Dev
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             this.ViewBag.cfdiService = ConfigurationManager.AppSettings["cfdiService"];
             this.ViewBag.cfdiServiceTimeSpan = SATManager.GetCFDIServiceTimeSpan().Minutes.ToString();
 
             return View();
+        }
+
+
+        public ActionResult GetTimbradoLog() {
+
+            CloudStorageAccount account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["AzureDefaultStorageConnectionString"]);
+            CloudBlobClient client = account.CreateCloudBlobClient();
+            CloudBlobContainer container = client.GetContainerReference(ConfigurationManager.AppSettings["AzureDefaultStorage"]);
+
+            var list = container.ListBlobs();
+            var itemList = new List<CloudStorageItem>();
+
+            var readPolicy = new Microsoft.WindowsAzure.Storage.Blob.SharedAccessBlobPolicy() {
+                Permissions = Microsoft.WindowsAzure.Storage.Blob.SharedAccessBlobPermissions.Read, // SharedAccessPermissions.Read,
+                SharedAccessExpiryTime = DateTime.UtcNow + TimeSpan.FromMinutes(10)
+            };
+
+            foreach (var blob in list.OfType<Microsoft.WindowsAzure.Storage.Blob.CloudBlockBlob>()) {
+                blob.FetchAttributes();
+                var item = new CloudStorageItem {
+                    Name = blob.Name,
+                    Url = new Uri(blob.Uri.AbsoluteUri + blob.GetSharedAccessSignature(readPolicy)).ToString(), //  blob.Uri.AbsolutePath
+                    ContentMD5 = blob.Properties.ContentMD5
+                };
+                itemList.Add(item);
+            }
+
+            var model = new GetTimbradoLogViewModel {
+                CloudStorageItems = itemList
+            };
+
+            return View(model);
         }
     }
 }
---
+++ src/Sistrategia.SAT.CFDiWebSite/Models/DevViewModels.cs
@@ -0,0 +1,13 @@
+﻿using Sistrategia.SAT.CFDiWebSite.CloudStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sistrategia.SAT.CFDiWebSite.Models
+{
+    public class GetTimbradoLogViewModel
+    {
+        public List<CloudStorageItem> CloudStorageItems { get; set; }        
+    }
+}
